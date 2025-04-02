07607ade19a03dfcd199717d81a9af55b9e0126e
unknown <nyakubyaraaline13@gmail.com>
2025-04-02 21:15:50 +0200

    my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@
 // import Form from './comps/Form';
 // import Array from "./array/Array";
 // import TryFile from "./array/tryFile";
-import HomePage from './pages/HomePage';
-
+// import HomePage from './pages/HomePage';
+// import File from "./comps/File";
+import Work from "./comps/Work";
 import './index.css';
 
 const App = ()=>
 {
   return(
     <>
-<HomePage/>
+<Work/>
+{/* <File/> */}
+{/* <HomePage/> */}
 {/* <Navbar /> */}
 {/* <TryFile/> */}
 {/* <Array /> */}
---
+++ src/comps/File.jsx
@@ -0,0 +1,18 @@
+function File()
+{
+    return(
+        <>
+        <div className="flex">
+            <div className="w-full">
+             <img src="" alt="" />
+            </div>
+            <div className="w-full p-10">
+                <h1 className="text-3xl font-bold mb-4">Instagram</h1>
+                <input type="text" className="w-full p-3" placeholder="Enter username/Email" />
+                <input type="text" className="w-full p-3" placeholder="Enter Password" />
+            </div>
+        </div>
+        </>
+    )
+}
+export default File
